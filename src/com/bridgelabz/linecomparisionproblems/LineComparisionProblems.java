0b58f09350052a3a7f2efe1fd8dac33864b3c38d
“swaranbhargav” <swaranbhargav54@gmail.com>
2023-03-23 08:18:28 +0530

    Add : Here we are finding the length of the line between two points

--- src/com/bridgelabz/linecomparisionproblems/LineComparisionProblems.java
+++ src/com/bridgelabz/linecomparisionproblems/LineComparisionProblems.java
@@ -4,6 +4,13 @@ public class LineComparisionProblems {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computation Program");
+		int x1 = 5;
+		int x2 = 6;
+		int y1 = 4;
+		int y2 = 7;
+		double lineLength;
+		lineLength = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+		System.out.println("The length of the line between twon points is " + lineLength);
 	}
 
 }
